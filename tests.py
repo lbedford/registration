cb1d019c503f5c143b2f76281048cf2d3d79ac96
Liam Bedford <lbedford@lbedford.org>
2018-10-08 15:25:10 +0200

    add more tests

--- tests.py
+++ tests.py
@@ -6,7 +6,9 @@ from django.db import IntegrityError
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from .models import Activity
 from .models import Lbw
+
 
 class LbwModelTest(TestCase):
     user = None
@@ -39,3 +41,89 @@ class LbwModelTest(TestCase):
         lbw = Lbw(start_date=start_date, end_date=end_date)
         lbw.clean()
         lbw.save()
+
+    def test_schedule_days_with_no_events(self):
+        start_date = datetime.now() + timedelta(weeks=3)
+        end_date = datetime.now() + timedelta(weeks=4)
+        lbw = Lbw(start_date=start_date, end_date=end_date)
+        lbw.save()
+        result = lbw.ScheduleDays()
+        self.assertEqual(8, len(result), 'Incorrect number of scheduled days.')
+
+    def test_schedule_days_with_events_before(self):
+        start_date = datetime.now() + timedelta(weeks=3)
+        end_date = datetime.now() + timedelta(weeks=4)
+        lbw = Lbw(start_date=start_date, end_date=end_date)
+        lbw.save()
+
+        activity_start_date = datetime.now() + timedelta(weeks=2)
+        activity = Activity(start_date=activity_start_date, duration=60)
+        activity.save()
+
+        lbw.activity.add(activity)
+        lbw.save()
+
+        result = lbw.ScheduleDays()
+
+        self.assertEqual(9, len(result), 'Incorrect number of scheduled days')
+
+    def test_schedule_days_with_events_after(self):
+        start_date = datetime.now() + timedelta(weeks=3)
+        end_date = datetime.now() + timedelta(weeks=4)
+        lbw = Lbw(start_date=start_date, end_date=end_date)
+        lbw.save()
+
+        activity_start_date = datetime.now() + timedelta(weeks=5)
+        activity = Activity(start_date=activity_start_date, duration=60)
+        activity.save()
+
+        lbw.activity.add(activity)
+        lbw.save()
+
+        result = lbw.ScheduleDays()
+
+        self.assertEqual(9, len(result), 'Incorrect number of scheduled days')
+
+    def test_schedule_days_with_events_before_and_after(self):
+        start_date = datetime.now() + timedelta(weeks=3)
+        end_date = datetime.now() + timedelta(weeks=4)
+        lbw = Lbw(start_date=start_date, end_date=end_date)
+        lbw.save()
+
+        activity_start_date = datetime.now() + timedelta(weeks=5)
+        activity = Activity(start_date=activity_start_date, duration=60)
+        activity.save()
+
+        lbw.activity.add(activity)
+
+        activity_start_date = datetime.now() + timedelta(weeks=2)
+        activity = Activity(start_date=activity_start_date, duration=60)
+        activity.save()
+
+        lbw.activity.add(activity)
+
+        lbw.save()
+
+        result = lbw.ScheduleDays()
+
+        self.assertEqual(10, len(result), 'Incorrect number of scheduled days')
+
+    def test_get_missing_users(self):
+        pass
+
+    def test_get_activities_per_day_by_time(self):
+        pass
+
+    def test_get_schedule(self):
+        pass
+
+    def test_unicode(self):
+        start_date = datetime.now() + timedelta(weeks=3)
+        end_date = datetime.now() + timedelta(weeks=4)
+        lbw = Lbw(start_date=start_date, end_date=end_date, short_name='Ilkley Moor', description='bar tat')
+        lbw.save()
+
+        self.assertEqual('Ilkley Moor', lbw.__unicode__(), 'unicode method got broken')
+        self.assertEqual('Ilkley Moor', lbw.__str__(), 'str method got broken')
+
+
